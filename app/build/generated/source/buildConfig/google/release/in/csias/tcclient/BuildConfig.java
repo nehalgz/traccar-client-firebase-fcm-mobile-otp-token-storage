e299d12f05cb4bed8867d6417c81f7d4b51f2bf4
nehalgz <nehalgamezone@gmail.com>
2022-05-20 12:31:54 +0530

    Another Release

---
+++ app/build/generated/source/buildConfig/google/release/in/csias/tcclient/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package in.csias.tcclient;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "in.csias.tcclient";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "google";
+  public static final int VERSION_CODE = 84;
+  public static final String VERSION_NAME = "6.23";
+  // Field from default config.
+  public static final boolean HIDDEN_APP = false;
+}
---
+++ app/build/generated/source/buildConfig/regular/release/in/csias/tcclient/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package in.csias.tcclient;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "in.csias.tcclient";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "regular";
+  public static final int VERSION_CODE = 83;
+  public static final String VERSION_NAME = "6.22";
+  // Field from default config.
+  public static final boolean HIDDEN_APP = false;
+}
